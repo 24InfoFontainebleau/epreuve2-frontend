f5074540fac0f6d2ff85dc93b0cf4d4e20e00370
Marco Orfao <marco.orfao@etu.u-pec.fr>
2025-05-24 00:19:12 +0200

    updated : main page, added : image and hover animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,22 @@
 import React from "react";
 import styles from "@/scss/pages/main.module.scss";
+import Link from "next/link";
+import frereLumiere from "@/assets/frereLumiere.jpg";
 
 export default function Home() {
   return (
     <React.Fragment>
       <div className={styles.pageContent}>
-        <div className={`${styles.card} ${styles.lumiereCard}`}></div>
+        <Link
+          href="/lumiere"
+          className={`${styles.card} ${styles.lumiereCard}`}
+          style={{ backgroundImage: `url(${frereLumiere.src})` }}
+        >
+          <h1 className={styles.cardTitle}>Les frères Lumières</h1>
+        </Link>
         <div className={`${styles.card} ${styles.partyCard}`}></div>
-        <div className={`${styles.card} ${styles.lumiereCard}`}></div>
-        <div className={`${styles.card} ${styles.lumiereCard}`}></div>
+        <div className={`${styles.card} ${styles.cineCard}`}></div>
+        <div className={`${styles.card} ${styles.philoCard}`}></div>
       </div>
     </React.Fragment>
   );
