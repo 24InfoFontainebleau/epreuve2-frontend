f26bee7ec4c8f0f6c7a25ef3d2210c51d35ed7f9
GalacticKitchen <136994993+Galactic-Kitchen@users.noreply.github.com>
2025-05-24 00:56:23 +0200

    added: link homescreen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import styles from "@/scss/pages/main.module.scss";
 import Link from "next/link";
 import frereLumiere from "@/assets/frereLumiere.jpg";
+import feteLumiere from "@/assets/feteLumiere.jpg";
+import cineconcert from "@/assets/cineconcert.jpg";
 
 export default function Home() {
   return (
@@ -14,9 +16,23 @@ export default function Home() {
         >
           <h1 className={styles.cardTitle}>Les frères Lumières</h1>
         </Link>
-        <div className={`${styles.card} ${styles.partyCard}`}></div>
-        <div className={`${styles.card} ${styles.cineCard}`}></div>
-        <div className={`${styles.card} ${styles.philoCard}`}></div>
+        <Link
+          href="/fete"
+          className={`${styles.card} ${styles.partyCard}`}
+          style={{ backgroundImage: `url(${feteLumiere.src})` }}
+        >
+          <h1 className={styles.cardTitle}>La fête des lumières</h1>
+        </Link>
+        <div className={`${styles.card} ${styles.cineCard}`}>
+          <h1>Theme to find</h1>
+        </div>
+        <Link
+          href="/cineconcert"
+          className={`${styles.card} ${styles.philoCard}`}
+          style={{ backgroundImage: `url(${cineconcert.src})` }}
+        >
+          <h1 className={styles.cardTitle}>Les cinéconcerts de Lyon</h1>
+        </Link>
       </div>
     </React.Fragment>
   );
