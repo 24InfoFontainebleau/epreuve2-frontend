4f0bd7521097fecb8de529e7d74ee22f735f65c4
hadjem-b <alexis.hadjembrancher@gmail.com>
2025-05-24 00:45:15 +0200

    ajout qcm

--- src/app/qcm/qcm.ts
+++ src/app/qcm/qcm.ts
@@ -1,19 +1,65 @@
 export const lumiere = [
   {
-    question: "...",
+    question: "À votre avis, combien de film ont été produits en 2024 ? ",
     responses: [
       {
-        text: "...",
+        text: "Environ 1000",
+        correct: false,
+      },
+      {
+        text: "Entre 3000-4000",
+        correct: false,
+      },
+      {
+        text: "Entre 7000-8000",
         correct: true,
       },
       {
-        text: "...",
+        text: "Entre 12000-14000",
+        correct: false,
+      }
+    ],
+  },
+  {
+    question: "Commençons par le tout début, qui sont fondateur du cinéma ?",
+    responses: [
+      {
+        text: "Les frères lumières",
+        correct: true,
+      },
+      {
+        text: "Les frères Tesla",
         correct: false,
       },
       {
-        text: "...",
+        text: "Les frères Mbappé",
         correct: false,
       },
+      {
+        text: "La famille Joestar",
+        correct: false,
+      }
     ],
   },
+  {
+    question: "Quand a-t-il été présenter pour la première fois ? ",
+    responses: [
+      {
+        text: "1852",
+        correct: true,
+      },
+      {
+        text: "1930",
+        correct: false,
+      },
+      {
+        text: "1896",
+        correct: false,
+      },
+      {
+        text: "1908",
+        correct: false,
+      }
+    ],
+  }
 ];
