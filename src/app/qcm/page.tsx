2bdb2305ba22eb83e42cb9157b1464fa3f1fce50
Marco Orfao <marco.orfao@etu.u-pec.fr>
2025-05-24 00:32:31 +0200

    added : data file for QCM and QCM page

---
+++ src/app/qcm/page.tsx
@@ -0,0 +1,10 @@
+import styles from "@/scss/pages/qcm.module.scss";
+import React from "react";
+
+export default function QCM() {
+  return (
+    <React.Fragment>
+      <div className={styles.pageContent}></div>
+    </React.Fragment>
+  );
+}
---
+++ src/app/qcm/qcm.ts
@@ -0,0 +1,19 @@
+export const lumiere = [
+  {
+    question: "...",
+    responses: [
+      {
+        text: "...",
+        correct: true,
+      },
+      {
+        text: "...",
+        correct: false,
+      },
+      {
+        text: "...",
+        correct: false,
+      },
+    ],
+  },
+];
